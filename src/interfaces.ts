f50ec75c403cfd9b0693adf6a99ebbaba361f0d4
stastny1 <neos26908@gmail.com>
2022-12-11 14:02:35 +0100

    Some real good ts

---
+++ src/interfaces.ts
@@ -0,0 +1,17 @@
+export interface InputIds {
+  "x": string;
+  "y": string;
+  "vel_x": string;
+  "vel_y": string;
+  "width": string;
+  "color": string;
+}
+
+export interface RectData {
+  "x": number;
+  "y": number;
+  "vel_x": number;
+  "vel_y": number;
+  "width": number;
+  "color": string;
+}
